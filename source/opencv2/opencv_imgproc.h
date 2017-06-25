f80b63e51908f6479151835cacd31d2cefeaadbe
hihozhou <hihozhou@gmail.com>
2017-06-25 15:29:29 +0800

    color conversion code in CV\cvtColor

---
+++ source/opencv2/opencv_imgproc.h
@@ -0,0 +1,10 @@
+//
+// Created by hiho on 17-6-25.
+//
+
+#ifndef OPENCV_OPENCV_IMGPROC_H
+#define OPENCV_OPENCV_IMGPROC_H
+
+void opencv_color_conversion_code_init(int module_number);
+
+#endif //OPENCV_OPENCV_IMGPROC_H

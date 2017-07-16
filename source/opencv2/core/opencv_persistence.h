4dde99addc3d7a83ce1ffc8fb3851b8e68a1d468
hihozhou <hihozhou@gmail.com>
2017-07-16 21:17:00 +0800

    Add Apache2.0 license

--- source/opencv2/core/opencv_persistence.h
+++ source/opencv2/core/opencv_persistence.h
@@ -1,5 +1,21 @@
-#ifndef OPENCV_OPENCV_PERSISTENCE_H
-#define OPENCV_OPENCV_PERSISTENCE_H
+/*
+ +----------------------------------------------------------------------+
+ | PHP-OpenCV                                                           |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 2.0 of the Apache license,    |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available through the world-wide-web at the following url:           |
+ | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ | If you did not receive a copy of the Apache2.0 license and are unable|
+ | to obtain it through the world-wide-web, please send a note to       |
+ | hihozhou@gmail.com so we can mail you a copy immediately.            |
+ +----------------------------------------------------------------------+
+ | Author: HaiHao Zhou  <hihozhou@gmail.com>                            |
+ +----------------------------------------------------------------------+
+ */
+
+#ifndef PHP_OPENCV_PERSISTENCE_H
+#define PHP_OPENCV_PERSISTENCE_H
 
 #include "../../../php_opencv.h"
 

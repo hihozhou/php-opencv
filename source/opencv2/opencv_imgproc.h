a25bcae1aad7ece863012e69bb521e87ae98aac3
马来酸 <CS_MaleicAcid@163.com>
2017-08-13 21:43:44 +0800

    Update opencv_imgproc.h

--- source/opencv2/opencv_imgproc.h
+++ source/opencv2/opencv_imgproc.h
@@ -21,7 +21,6 @@ void opencv_imgproc_init(int module_number);
 void opencv_color_conversion_code_init(int module_number);
 void opencv_line_type_init(int module_number);
 PHP_FUNCTION(opencv_cv_t_color);
-PHP_FUNCTION(opencv_threshold);
 PHP_FUNCTION(opencv_ellipse);
 PHP_FUNCTION(opencv_circle);
 ZEND_BEGIN_ARG_INFO_EX(opencv_fill_poly_arginfo, 0, 0, 7)

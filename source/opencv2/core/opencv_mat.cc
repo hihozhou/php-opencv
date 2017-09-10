9b09e313a040db11e3115d505ba92296397b62de
hihozhou <hihozhou@gmail.com>
2017-09-10 17:23:11 +0800

    Add CV\Mat->setTo function

--- source/opencv2/core/opencv_mat.cc
+++ source/opencv2/core/opencv_mat.cc
@@ -531,6 +531,30 @@ PHP_METHOD(opencv_mat, divide)
     RETURN_ZVAL(&instance,0,0); //return php Mat object
 }
 
+
+
+/**
+ * //todo mask
+ * Mat->setTo(Scalar $value)
+ * @param execute_data
+ * @param return_value
+ */
+PHP_METHOD(opencv_mat, set_to)
+{
+    zval *value_zval;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "O", &value_zval, opencv_scalar_ce) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_mat_object *this_obj = Z_PHP_MAT_OBJ_P(getThis());
+    opencv_scalar_object *value_obj = Z_PHP_SCALAR_OBJ_P(value_zval);
+    this_obj->mat->setTo(*value_obj->scalar);
+
+    RETURN_NULL();
+
+}
+
 /**
  * opencv_mat_methods[]
  */
@@ -556,6 +580,7 @@ const zend_function_entry opencv_mat_methods[] = {
         PHP_MALIAS(opencv_mat, convertTo ,convert_to, opencv_mat_convert_to_arginfo, ZEND_ACC_PUBLIC)
         PHP_ME(opencv_mat, plus, NULL, ZEND_ACC_PUBLIC)
         PHP_ME(opencv_mat, divide, NULL, ZEND_ACC_PUBLIC)
+        PHP_MALIAS(opencv_mat, setTo ,set_to, NULL, ZEND_ACC_PUBLIC)
         PHP_FE_END
 };
 /* }}} */

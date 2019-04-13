5eb1d4e1d62adbc2c23fd9077fd502016270e322
tarasfrompir <tarasfrompir@inbox.ru>
2019-04-13 11:24:35 +0300

    Update opencv_objdetect.h

--- source/opencv2/opencv_objdetect.h
+++ source/opencv2/opencv_objdetect.h
@@ -25,7 +25,7 @@ extern zend_class_entry *opencv_cascade_classifier_ce;
 
 typedef struct _opencv_cascade_classifier_object{
     zend_object std;
-    CascadeClassifier *cascadeClassifier;
+    struct CascadeClassifier *cascadeClassifier;
 }opencv_cascade_classifier_object;
 
 extern void opencv_objdetect_init(int module_number);
